457ebfb630074fca3edc9ed93b86b68faacfceca
ayushjangida30 <ayush301099@gmail.com>
2024-09-09 14:31:44 -0700

    Changed repository name and added github pages

--- src/MyGame.js
+++ src/MyGame.js
@@ -6,23 +6,27 @@ import React, { useState } from 'react';
 
 function MyGame() {
     const [link, setLink] = useState("https://itch.io/embed-upload/10897415?color=333333");
+    const [number, setNumber] = useState(1);
 
     const handSetLink = (linkId) => {
         switch(linkId)  {
             case 1: 
                 setLink("https://itch.io/embed-upload/10897347?color=333333");
+                setNumber(1);
                 break;
 
             case 2:
                 setLink("https://itch.io/embed-upload/10897415?color=333333");
+                setNumber(2);
+                break;
         }
     };
 
     return (
     <div id='games' className='game-container'>
         <div className='mygame-heading-container'>
-            <p className='mygame-name' onClick={() => handSetLink(1)}>Falling Bricks</p>
-            <p className='mygame-name' onClick={() => handSetLink(2)}>Mole in a Hole</p>
+            <p className={number === 1 ? 'mygame-name-selected' : 'mygame-name'} onClick={() => handSetLink(1)}>Falling Bricks</p>
+            <p className={number === 2 ? 'mygame-name-selected' : 'mygame-name'} onClick={() => handSetLink(2)}>Mole in a Hole</p>
         </div>
         <div className='mygame-container'>
             <iframe 
